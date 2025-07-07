b3c74fae3016e79cec1ddf9c49919d2eaa0bf3fb
Dedealtograu <matias.jms14@gmail.com>
2025-07-07 11:25:20 -0300

    Final da aula 164

--- src/app/admin/post/page.tsx
+++ src/app/admin/post/page.tsx
@@ -1,9 +1,18 @@
+import PostsListAdmin from '@/components/PostsListAdmin'
+import { SpinLoader } from '@/components/SpinLoader'
+import { Metadata } from 'next'
+import { Suspense } from 'react'
+
 export const dynamic = 'force-dynamic'
+
+export const metadata: Metadata = {
+  title: 'Post Admin',
+}
 
 export default async function AdminPostPage() {
   return (
-    <div className='py-16 text-6xl'>
-      <h1>Admin Post</h1>
-    </div>
+    <Suspense fallback={<SpinLoader className='mb-16' />}>
+      <PostsListAdmin />
+    </Suspense>
   )
 }
--- src/repositories/post/drizzle-post-repository.ts
+++ src/repositories/post/drizzle-post-repository.ts
@@ -1,9 +1,15 @@
 import { PostModel } from '@/models/post/post-model'
 import { PostRepository } from './post-repository'
 import { drizzeDb } from '@/db/drizzle'
+import { logColor } from '@/utils/log-color'
+import { asyncDelay } from '@/utils/async-delay'
+import { SIMULATE_WAIT_IN_MS } from '@/lib/constants'
 
 export class DrizzlePostRepository implements PostRepository {
   async findAllPublic(): Promise<PostModel[]> {
+    await asyncDelay(SIMULATE_WAIT_IN_MS, true)
+    logColor('findAllPublic', Date.now())
+
     const posts = await drizzeDb.query.posts.findMany({
       orderBy: (posts, { desc }) => desc(posts.published),
       where: (posts, { eq }) => eq(posts.published, true),
@@ -13,6 +19,9 @@ export class DrizzlePostRepository implements PostRepository {
   }
 
   async findBySlugPublic(slug: string): Promise<PostModel> {
+    await asyncDelay(SIMULATE_WAIT_IN_MS, true)
+    logColor('findBySlugPublic', Date.now())
+
     const post = await drizzeDb.query.posts.findFirst({
       where: (posts, { eq, and }) => and(eq(posts.published, true), eq(posts.slug, slug))
     })
@@ -24,6 +33,9 @@ export class DrizzlePostRepository implements PostRepository {
   }
 
   async findAll(): Promise<PostModel[]> {
+    await asyncDelay(SIMULATE_WAIT_IN_MS, true)
+    logColor('findAll', Date.now())
+
     const posts = await drizzeDb.query.posts.findMany({
       orderBy: (posts, { desc }) => desc(posts.createdAt),
     })
@@ -32,6 +44,9 @@ export class DrizzlePostRepository implements PostRepository {
   }
 
   async findById(id: string): Promise<PostModel> {
+    await asyncDelay(SIMULATE_WAIT_IN_MS, true)
+    logColor('findById', Date.now())
+
     const post = await drizzeDb.query.posts.findFirst({
       where: (posts, { eq }) => eq(posts.id, id)
     })
@@ -40,7 +55,6 @@ export class DrizzlePostRepository implements PostRepository {
 
     return post
   }
-
 }
 
 // (async () => {
