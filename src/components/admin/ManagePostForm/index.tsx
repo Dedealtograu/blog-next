ab97af2ea6f90fd99bf9197c3033928cb37c5855
Dedealtograu <matias.jms14@gmail.com>
2025-07-11 12:02:47 -0300

    Final da aula 180

--- src/components/admin/ManagePostForm/index.tsx
+++ src/components/admin/ManagePostForm/index.tsx
@@ -3,14 +3,31 @@
 import { Button } from '@/components/Button'
 import { InputCheckBox } from '@/components/InputCheckBox'
 import { InputText } from '@/components/InputText'
+import { MarkdownEditor } from '@/components/MarkdownEditor'
+import { useState } from 'react'
+import { ImageUploader } from '../ImageUploader'
 
 export function ManagePostForm() {
+  const [contentValue, setContentValue] = useState('')
+
   return (
     <form action='' className='mb-16'>
       <div className='flex flex-col gap-6'>
         <InputText labelText='nome' placeholder='Digite seu nome' />
+
+        <ImageUploader />
+
         <InputText labelText='sobrenome' placeholder='Digite seu sobrenome' />
         <InputCheckBox labelText='publicado' />
+
+        <MarkdownEditor
+          labelText='Conteúdo'
+          disabled={false}
+          textAreaName='content'
+          value={contentValue}
+          setValue={setContentValue}
+        />
+
         <div className='mt-4'>
           <Button type='submit'>Enviar</Button>
         </div>
