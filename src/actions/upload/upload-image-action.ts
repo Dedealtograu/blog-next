ec8a8544eb4a3dae1176ae64598e9b205ded7361
Dedealtograu <matias.jms14@gmail.com>
2025-07-15 12:23:05 -0300

    Final da aula 187

--- src/actions/upload/upload-image-action.ts
+++ src/actions/upload/upload-image-action.ts
@@ -1,9 +1,50 @@
 'use server'
 
-export async function uploadImageAction() {
-  console.log('Olá da action uploadImageAction')
+import { IMAGE_UPLOAD_DIRECTORY, IMAGE_UPLOADER_MAX_SIZE } from '@/lib/constants'
+import { mkdir, writeFile } from 'fs/promises'
+import { extname, resolve } from 'path'
 
-  return {
-    user: 'SENHA DO USUÁRIO',
+type uploadImageActionResylt = {
+  url: string
+  error: string
+}
+
+
+export async function uploadImageAction(formData: FormData): Promise<uploadImageActionResylt> {
+
+  const makeResult = ({url = '', error = ''}) => ({url, error})
+
+  if (!(formData instanceof FormData)) {
+    return makeResult({ error: 'Dados inválidos' })
   }
+
+  const file = formData.get('file')
+
+  if (!(file instanceof File)) {
+    return makeResult({ error: 'Arquivo inválido' })
+  }
+
+  if (file.size > IMAGE_UPLOADER_MAX_SIZE) {
+    return makeResult({ error: 'Arquivo muito grande' })
+  }
+
+  if (!file.type.startsWith('image/')) {
+    return makeResult({ error: 'Imagem inválida' })
+  }
+
+  const imageExtension = extname(file.name)
+  const uniqueImageName = `${Date.now()}${imageExtension}`
+  const uploadFullPath = resolve(process.cwd(), 'public', IMAGE_UPLOAD_DIRECTORY)
+
+  await mkdir(uploadFullPath, { recursive: true })
+
+  const fileArrayBuffer = await file.arrayBuffer()
+  const buffer = Buffer.from(fileArrayBuffer)
+  const fileFullPath = resolve(uploadFullPath, uniqueImageName)
+
+  await writeFile(fileFullPath, buffer)
+
+  const url = `http://localhost:3000/${IMAGE_UPLOAD_DIRECTORY}/${uniqueImageName}`
+
+  return makeResult({ url })
 }
--- src/components/admin/ImageUploader/index.tsx
+++ src/components/admin/ImageUploader/index.tsx
@@ -4,12 +4,13 @@ import { uploadImageAction } from '@/actions/upload/upload-image-action'
 import { Button } from '@/components/Button'
 import { IMAGE_UPLOADER_MAX_SIZE } from '@/lib/constants'
 import { ImageUpIcon } from 'lucide-react'
-import { useRef, useTransition } from 'react'
+import { useRef, useState, useTransition } from 'react'
 import { toast } from 'react-toastify'
 
 export function ImageUploader() {
   const fileInputRef = useRef<HTMLInputElement>(null)
   const [isUploading, startTransition] = useTransition()
+  const [imgUrl, setImgUrl] = useState('')
 
   function handleChooseFile() {
     if (!fileInputRef.current) return
@@ -17,19 +18,27 @@ export function ImageUploader() {
   }
 
   function handleChange() {
-    if (!fileInputRef.current) return
+    toast.dismiss()
+
+    if (!fileInputRef.current) {
+      setImgUrl('')
+      return
+    }
 
     const fileInput = fileInputRef.current
     const file = fileInput?.files?.[0]
 
-    if (!file) return
+    if (!file) {
+      setImgUrl('')
+      return
+    }
 
     if (file.size > IMAGE_UPLOADER_MAX_SIZE) {
       const readableFileSize = IMAGE_UPLOADER_MAX_SIZE / 1024
       toast.error(`A imagem deve ter no máximo ${readableFileSize}KB`)
 
       fileInput.value = ''
-
+      setImgUrl('')
       return
     }
 
@@ -37,18 +46,38 @@ export function ImageUploader() {
     formData.append('file', file)
 
     startTransition(async () => {
-      const result = await uploadImageAction()
+      const result = await uploadImageAction(formData)
+
+      if (result.error) {
+        toast.error(result.error)
+        fileInput.value = ''
+        setImgUrl('')
+        return
+      }
+
+      setImgUrl(result.url)
+
+      toast.success('Imagem enviada')
     })
 
     fileInput.value = ''
   }
 
   return (
-    <div className='flex flex-col gap-2 py-4'>
-      <Button onClick={handleChooseFile} type='button' className='self-start'>
+    <div className='flex flex-col gap-4 py-4'>
+      <Button onClick={handleChooseFile} type='button' className='self-start' disabled={isUploading}>
         <ImageUpIcon />
         Enviar uma imagem
       </Button>
+      {!!imgUrl && (
+        <div className='flex flex-col gap-4'>
+          <p>
+            <b>URL:</b> {imgUrl}
+          </p>
+          {/* eslint-disable-next-line */}
+          <img className='rounded-lg' src={imgUrl} alt='Teste' />
+        </div>
+      )}
       <input onChange={handleChange} ref={fileInputRef} className='hidden' name='file' type='file' accept='image/*' />
     </div>
   )
